acde169188bd3e3cf624846108d9c9cc0c81191b
Ivan Duran <ivan@ibm.com>
2021-11-24 17:36:32 -0500

    Add job_ids() function to the backend
    - Maybe it is not the most appropriate name for the function... but we could change it in the next future

--- qiskit_ibm/api/clients/account.py
+++ qiskit_ibm/api/clients/account.py
@@ -174,6 +174,27 @@ class AccountClient(BaseClient):
         return self.account_api.jobs(limit=limit, skip=skip, descending=descending,
                                      extra_filter=extra_filter)
 
+    def list_jobs_id(
+            self,
+            limit: int = 10,
+            skip: int = 0,
+            descending: bool = True,
+            extra_filter: Optional[Dict[str, Any]] = None
+    ) -> List[Dict[str, Any]]:
+        """Return a list of job id, with filtering and pagination.
+
+        Args:
+            limit: Maximum number of items to return.
+            skip: Offset for the items to return.
+            descending: Whether the jobs should be in descending order.
+            extra_filter: Additional filtering passed to the query.
+
+        Returns:
+            A list of job data.
+        """
+        return self.account_api.jobs_id(limit=limit, skip=skip, descending=descending,
+                                     extra_filter=extra_filter)
+
     def job_submit(
             self,
             backend_name: str,
